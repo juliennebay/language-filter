5353964fc5cfd75c15e01f90ce2d9ff1cf8edd09
Julie <julienne.bay@gmail.com>
2023-01-07 18:57:31 -0500

    Part A - implement /repos endpoint Return only unforked repositories

--- api/src/routes/repos.js
+++ api/src/routes/repos.js
@@ -1,12 +1,23 @@
 import { Router, Request, Response } from 'express';
+import axios from 'axios';
 
 export const repos = Router();
 
-repos.get('/', async (_: Request, res: Response) => {
+repos.get('/', async (_, res) => {
   res.header('Cache-Control', 'no-store');
+
+  const silverOrangeRepos = await axios
+    .get('https://api.github.com/users/silverorange/repos')
+    .catch(({ response: { status, statusText } }) => {
+      res.status(status).send(`${status}: ${statusText}`);
+    });
+
+  const unforkedRepos = silverOrangeRepos.data.filter(
+    (repo) => repo.fork === false
+  );
 
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  res.json(unforkedRepos);
 });
