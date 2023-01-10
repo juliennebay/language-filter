bfa1a9d65274a2685cc035d47cddafef53ec7dad
Julie <julienne.bay@gmail.com>
2023-01-10 10:40:42 -0500

    show a message while loading items

--- web/src/components/List.js
+++ web/src/components/List.js
@@ -5,6 +5,7 @@ import Header from './Header';
 function List() {
   const [items, setItems] = useState([]);
   const [filteredItems, setFilteredItems] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   //****note to self - error handling */
   useEffect(() => {
@@ -13,46 +14,51 @@ function List() {
 
       setItems(result);
       setFilteredItems(result);
+      setLoading(true);
     };
 
     dataFetch();
   }, []);
 
-  return (
-    <Fragment>
-      <Header
-        filter={(language) =>
-          setFilteredItems(items.filter((item) => item.language === language))
-        }
-        reset={() => setFilteredItems(items)}
-      />
-      <div>
-        {filteredItems.length !== 0 ? (
-          <ul>
-            {filteredItems
-              .sort((a, b) =>
-                b.created_at > a.created_at
-                  ? 1
-                  : b.created_at < a.created_at
-                  ? -1
-                  : 0
-              )
-              .map((item) => (
-                <li key={item.id}>
-                  <div>Repository name: {item.name}</div>
-                  <div>Description: {item.description}</div>
-                  <div>Language: {item.language}</div>
-                  <div>Forks count: {item.forks_count}</div>
-                  <div>Creation date - remove later: {item.created_at}</div>
-                </li>
-              ))}
-          </ul>
-        ) : (
-          <div>No repo found under this language</div>
-        )}
-      </div>
-    </Fragment>
-  );
+  if (!loading) {
+    return <div>Loading...please wait</div>;
+  } else {
+    return (
+      <Fragment>
+        <Header
+          filter={(language) =>
+            setFilteredItems(items.filter((item) => item.language === language))
+          }
+          reset={() => setFilteredItems(items)}
+        />
+        <div>
+          {filteredItems.length !== 0 ? (
+            <ul>
+              {filteredItems
+                .sort((a, b) =>
+                  b.created_at > a.created_at
+                    ? 1
+                    : b.created_at < a.created_at
+                    ? -1
+                    : 0
+                )
+                .map((item) => (
+                  <li key={item.id}>
+                    <div>Repository name: {item.name}</div>
+                    <div>Description: {item.description}</div>
+                    <div>Language: {item.language}</div>
+                    <div>Forks count: {item.forks_count}</div>
+                    <div>Creation date - remove later: {item.created_at}</div>
+                  </li>
+                ))}
+            </ul>
+          ) : (
+            <div>No repo found under this language</div>
+          )}
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default List;
